6bd34d1ea371af87603853d36f0489a835f6f86f
Rh4yanna <rhayannaborges6@gmail.com>
2026-05-15 13:33:33 -0300

    corrigindo pags publicas

---
+++ src/pagespub/detalheProj.jsx
@@ -0,0 +1,9 @@
+function DetalheProj() {
+  return (
+    <div>
+      <h1>Detalhes Projeto</h1>
+    </div>
+  );
+}
+
+export default DetalheProj;
---
+++ src/pagespub/portifolio.jsx
@@ -0,0 +1,9 @@
+function Portfolio() {
+  return (
+    <div>
+      <h1>Portfólio</h1>
+    </div>
+  );
+}
+
+export default Portfolio;
